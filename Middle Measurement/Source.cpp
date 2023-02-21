621a0fa436fdffa2f3614d0bf1c61ddf9348b8eb
VitalySerdechny <serdechny_family_ua@outlook.com>
2023-02-21 12:53:05 +0200

    Some changes

--- Middle Measurement/Source.cpp
+++ Middle Measurement/Source.cpp
@@ -6,55 +6,57 @@ using std::cin;
 using std::endl;
 
 int main() {
-    cout << "Data Mining Statistic Description" << endl;
-    cout << "Author: Serdechny V.S." << endl;
-    cout << "ITSTEP University, 2023\n\n";
+    setlocale(LC_ALL, "Ukrainian");
+
+    cout << "ИАД. Статическое описание данных" << endl;
+    cout << "Автор: Сердечный В.С." << endl;
+    cout << "Харьковский технологический университет, 2023\n\n";
 
     const int data_size = 12;
     int data[data_size];
 
     rand_integer_data(data, data_size, 1000, 2000);
-    cout << "Source data (salaries): ";
+    cout << "Исходные данные (зарплаты работников): ";
     print_integer_data(data, data_size);
 
-    cout << "\n\n1. Measures of the central tendency demonstation:";
-    cout << "\n\nArithmetic mean: " << arithmetic_mean(data, data_size) << endl;
-    cout << "Trimmed mean: " << trimmed_mean(data, data_size) << endl;
+    cout << "\n\n1. Измерение центральной тенденции:";
+    cout << "\n\nСреднее арифметическое: " << arithmetic_mean(data, data_size) << endl;
+    cout << "Обрезанное среднее арифметическое: " << trimmed_mean(data, data_size) << endl;
 
-    cout << "\nNow sorting source data..." << endl;
-    cout << "Result: ";
+    cout << "\nСортируем данные..." << endl;
+    cout << "Результат: ";
     sort(data, data_size);
     print_integer_data(data, data_size);
 
-    cout << "\n\nMedian: " << median(data, data_size) << endl;
+    cout << "\n\nМедиана: " << median(data, data_size) << endl;
 
     std::vector<int> m_d;
     int freq = 0;
     if (mode(data, data_size, m_d, freq))
     {
-        cout << "Modes: ";
+        cout << "Мода: ";
         for (int i = 0; i < m_d.size(); i++)
             cout << m_d.at(i) << " ";
-        cout << " (" << freq << " times)" << endl;
+        cout << " (" << freq << " раз)" << endl;
     }
     else
-        cout << "There is no mode in the set!\n";
+        cout << "Мода не обнаружена!\n";
 
-    cout << "Midrange: " << midrange(data, data_size) << endl;
+    cout << "Усредненный диапазон (midrange): " << midrange(data, data_size) << endl;
 
-    cout << "\n\n2. Measures of Dispersion:\n" << endl;
-    cout << "Range: " << range(data, data_size) << endl;
+    cout << "\n\n2. Измерение дисперсии:\n" << endl;
+    cout << "Диапазон: " << range(data, data_size) << endl;
 
     m_d.clear();
     calculate_quartiles(data, data_size, m_d);
-    cout << "\nQuartiles (IQR = " << m_d.at(2) - m_d.at(0)<< "):" << endl;
+    cout << "\nКвартили (IQR = " << m_d.at(2) - m_d.at(0)<< "):" << endl;
     for (int i = 0, percent = 25; i < m_d.size(); i++, percent += 25)
-        cout << endl << percent << "% quantile: " << m_d.at(i);
+        cout << endl << percent << "% квартиль: " << m_d.at(i);
     cout << endl;
 
-    cout << endl << "Outliers (";
-    cout << "High range (Q3+1.5xIQR): " << 1.5 * (m_d.at(2) - m_d.at(0)) + m_d.at(2);
-    cout << ", low range (Q1-1.5xIQR): " << m_d.at(0) - 1.5 * (m_d.at(2) - m_d.at(0)) << "): " << endl;
+    cout << endl << "Выбросы (аномалии) (";
+    cout << "Максимально допустимый предел (Q3+1.5xIQR): " << 1.5 * (m_d.at(2) - m_d.at(0)) + m_d.at(2);
+    cout << ", минимально допустимый предел (Q1-1.5xIQR): " << m_d.at(0) - 1.5 * (m_d.at(2) - m_d.at(0)) << "): " << endl;
     for (int i = 0; i < data_size; i++)
     {
         //1.5*IQR > Q3
@@ -66,10 +68,11 @@ int main() {
     }
     cout << endl;
 
-    cout << "Five-number summary: " << data[min_pos(data, data_size)] << ", ";
+    cout << "Пять значений описания дисперсии (five-number summary): " << data[min_pos(data, data_size)] << ", ";
     cout << m_d[0] << ", " << m_d[1] << ", " << m_d[2] << ", " << data[max_pos(data, data_size)] << endl;
 
     double v = variance(data, data_size);
-    cout << "\nVariance: " << v << endl;
-    cout << "Standart Deviation: " << sqrt(v) << endl;
+    cout << "\nДисперсия (variance): " << v << endl;
+    cout << "Стндартное отклонение (standart Deviation): " << sqrt(v) << endl;
+    cout << "Коэффициент девиации (Coefficient of Variation, CV): " << sqrt(v) / arithmetic_mean(data, data_size) << endl;
  } 
--- Middle Measurement/data_mining_utils.cpp
+++ Middle Measurement/data_mining_utils.cpp
@@ -4,42 +4,42 @@
 
 //100 500 300 200
 
-double variance(const int* arr, int size)
+double variance(const int* arr, size_t size)
 {
     double mean = arithmetic_mean(arr, size);
     double sum = 0;
-    for (int i = 0; i < size; i++)
+    for (size_t i = 0; i < size; i++)
         sum += pow(arr[i],2);
     return (sum / size) - pow(mean, 2);
 }
 
-void calculate_quartiles(const int* arr, int size, std::vector<int>& result)
+void calculate_quartiles(const int* arr, size_t size, std::vector<int>& result)
 {
-    for (int i = 25; i <= 75; i += 25)
+    for (size_t i = 25; i <= 75; i += 25)
         result.push_back(arr[((i * size) / 100)]);
 }
 
-int range(const int* arr, int size)
+int range(const int* arr, size_t size)
 {
     return arr[max_pos(arr, size)] - arr[min_pos(arr, size)];
 }
 
 //*****************************************************
 
-double midrange(const int* arr, int size)
+double midrange(const int* arr, size_t size)
 {
     return (arr[min_pos(arr, size)] + arr[max_pos(arr, size)]) / 2.0;
 }
  
-bool mode(const int* arr, int size, std::vector<int>& modes, int& frequency)
+bool mode(const int* arr, size_t size, std::vector<int>& modes, int& frequency)
 {
     int best_frequency_value_pos = -1;
     int max_count = 0;
 
-    for (int i = 0; i < size; i++)
+    for (size_t i = 0; i < size; i++)
     {
         int count = 0;
-        for (int j = 0; j < size; j++)
+        for (size_t j = 0; j < size; j++)
             if (arr[i] == arr[j])
                 count++;
         if (count > max_count)
@@ -56,7 +56,7 @@ bool mode(const int* arr, int size, std::vector<int>& modes, int& frequency)
         for (int i = 0; i < size; i++)
         {
             int count = 0;
-            for (int j = 0; j < size; j++)
+            for (size_t j = 0; j < size; j++)
                 if (arr[i] == arr[j])
                     count++;
             if (count == max_count && 
@@ -68,7 +68,7 @@ bool mode(const int* arr, int size, std::vector<int>& modes, int& frequency)
     }
 }
 
-double median(int* arr, int size)
+double median(int* arr, size_t size)
 {
     if (size % 2 != 0)
         return arr[size / 2];
@@ -76,27 +76,27 @@ double median(int* arr, int size)
         return (arr[size / 2] + arr[size / 2 - 1]) / 2.0;
 }
 
-double trimmed_mean(int* arr, int size, double percent_ignored) {
+double trimmed_mean(int* arr, size_t size, double percent_ignored) {
     int ignored_count = (percent_ignored * size) / 100;
     int sum = 0;
-    for (int i = ignored_count; i < size - ignored_count; i++)
+    for (size_t i = ignored_count; i < size - ignored_count; i++)
         sum += arr[i];
     return (double)sum / (size - ignored_count);
 }
 
-double arithmetic_mean(const int* arr, int size) {
+double arithmetic_mean(const int* arr, size_t size) {
     int sum = 0;
-    for (int i = 0; i < size; i++)
+    for (size_t i = 0; i < size; i++)
         sum += arr[i];
     return (double)sum / size;
 }
 
 //********************************************
 
-void sort(int* arr, int size) {
-    for (int i = 0; i < size; i++) {
+void sort(int* arr, size_t size) {
+    for (size_t i = 0; i < size; i++) {
         bool is_sorted = true;
-        for (int j = 0; j < size - 1; j++) {
+        for (size_t j = 0; j < size - 1; j++) {
             if (arr[j] > arr[j + 1]) {
                 std::swap(arr[j], arr[j + 1]);
                 is_sorted = false;
@@ -107,32 +107,32 @@ void sort(int* arr, int size) {
     }
 }
 
-int min_pos(const int* arr, int size)
+int min_pos(const int* arr, size_t size)
 {
     int imin = 0;
-    for (int i = 1; i < size; i++)
+    for (size_t i = 1; i < size; i++)
         if (arr[i] < arr[imin]) imin = i;
     return imin;
 }
 
-int max_pos(const int* arr, int size)
+int max_pos(const int* arr, size_t size)
 {
     int imax = 0;
-    for (int i = 1; i < size; i++)
+    for (size_t i = 1; i < size; i++)
         if (arr[i] > arr[imax]) imax = i;
     return imax;
 }
 
-void rand_integer_data(int* arr, int size, int minrnd, int maxrnd) {
+void rand_integer_data(int* arr, size_t size, int minrnd, int maxrnd) {
     if (arr == nullptr)
         return;
-    for (int i = 0; i < size; i++)
+    for (size_t i = 0; i < size; i++)
         arr[i] = minrnd + rand() % (maxrnd - minrnd + 1);
 }
 
-void print_integer_data(const int* arr, int size) {
+void print_integer_data(const int* arr, size_t size) {
     if (arr == nullptr)
         return;
-    for (int i = 0; i < size; i++)
+    for (size_t i = 0; i < size; i++)
         std::cout << arr[i] << " ";
 }
